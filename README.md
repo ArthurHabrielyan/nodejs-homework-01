0c3883dcc7c4acc3b96678b07831d585d2eec56f
Arthur Habrielyan <zkdzkd227@gmail.com>
2022-07-29 15:10:06 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # nodejs-homework-01
+
+list - https://monosnap.com/file/sxAOWmvKx0YLGuDTnpagzI3lQvWKx5
+
+getById - https://monosnap.com/file/j2fJRadSnfKsBY73eRd4dBNN0xzwdr
+
+add - https://monosnap.com/file/UWlgBqzdsbQJbVYhvtYTSkOY5e97WL
+
+remove - https://monosnap.com/file/jW3TkB1ub5ixRC8bfqIh9YnmIgfGjl
---
+++ contacts.js
@@ -0,0 +1,49 @@
+const fs = require("fs/promises");
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
+
+const contactsPath = path.join(__dirname, "db", "contacts.json");
+
+const updateContacts = async (contacts) => {
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+};
+
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+};
+
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const result = contacts.find((item) => item.id === contactId);
+  if (!result) {
+    return null;
+  }
+  return result;
+};
+
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const idx = contacts.findIndex((item) => item.id === contactId);
+  if (idx === -1) {
+    return null;
+  }
+  const [removeContact] = contacts.splice(idx, 1);
+  updateContacts(contacts);
+  return removeContact;
+};
+
+const addContact = async (name, email, phone) => {
+  const contacts = await listContacts();
+  const contact = { id: uuidv4(), name, email, phone };
+  contacts.push(contact);
+  await updateContacts(contacts);
+  return contact;
+};
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
