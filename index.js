f6b7b52b084ead0902041cc67c1062b4e7e9799b
Arthur Habrielyan <zkdzkd227@gmail.com>
2022-08-01 12:05:30 +0300

    add try/catch

--- index.js
+++ index.js
@@ -39,4 +39,12 @@ const invokeAction = async ({ action, id, name, email, phone }) => {
   }
 };
 
-invokeAction(argv);
+const start = async (argv) => {
+  try {
+    await invokeAction(argv);
+  } catch (error) {
+    console.log("Oops, something went wrong");
+  }
+};
+
+start(argv);
